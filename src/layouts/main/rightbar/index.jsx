c492cb0174d574af04acefe02795692a2373eab6
nusretkatirci <nusretktrc57@gmail.com>
2023-11-29 23:46:14 +0300

    29.11.2023

--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,7 +1,9 @@
+import Search from "~/layouts/main/rightbar/search/index.jsx";
+
 export default function RightBar(){
     return(
         <aside className="w-[350px] mr-2.5">
-            sağ sidebar
+            <Search/>
         </aside>
     )
 }
